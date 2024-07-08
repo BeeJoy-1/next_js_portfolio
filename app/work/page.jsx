11e8f57fc7dd0c1210e0cb4c450165f819bb096b
BeeJoy-1 <beejoy3000@gmail.com>
2024-07-08 11:56:13 +0600

    completed the tasks

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,8 @@
 "use client"
 
 import { motion } from "framer-motion"
-import React, { useState } from "react"
+// import React, { useState } from "react"
+import { useState } from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css";
